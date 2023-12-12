63ba020b9edda4510474b24cc53268b8870c05ba
M3PH1ST023 <mephisto23.03.2003@gmail.com>
2023-12-12 12:06:03 +0530

    changes

--- server/py-files/ip.py
+++ server/py-files/ip.py
@@ -3,7 +3,7 @@ from flask import jsonify
 from flask_cors import cross_origin
 from getmac import get_mac_address as gma
 import subprocess
-import netifaces as ni
+import requests
 
 @cross_origin(origin="http://127.0.0.1:5173")
 def getIp():
@@ -11,20 +11,38 @@ def getIp():
     hostname = socket.gethostname()
     # mac address
     mac_address = gma()
+    # ip address
+    url = 'https://api.ipify.org'
+    resp = requests.get(url)
+    ip = resp.text
     # connection name
     connection_name = "No connection"
+    connection_interface = ""
     try:
         # Run the iwconfig command to get details about the connected WiFi network
         connection_name = subprocess.check_output(['iwgetid']).decode().strip()
+        for i in connection_name:
+            if i != ' ':
+                connection_interface += i
+            else:
+                break;
     except subprocess.CalledProcessError as e:
         print("Error:", e)
-    
+    temp = ""
+    flag=0
+    for i in connection_name:
+        if i == '\"':
+            flag=1
+        if flag == 1:
+            temp+=i
+    connection_name = temp[1:len(temp)-1]
     # json
     json = {
+        'ip':str(ip),
         'hostname':str(hostname),
         'macAddress':str(mac_address),
         'connectionName':str(connection_name),
-        'connectionInterface': str(connection_name)
+        'connectionInterface': str(connection_interface)
     }
 
     return jsonify(json)
